ee0dd6b30db12e47289647e05f13cbda43012993
Bing Leng <icyice80@gmail.com>
2019-01-16 11:43:45 -0800

    add additional logging and enable redis cache

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 #!/usr/bin/env node
-var prerender = require('./lib');
 
-var server = prerender();
+const prerender = require('./lib');
+
+const options = {
+    logRequests: process.env.NODE_ENV !== "production" ? true : false
+};
+
+const server = prerender(options);
 
 server.use(prerender.sendPrerenderHeader());
+server.use(prerender.healthCheck());
 // server.use(prerender.blockResources());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
+
+if (process.env.REDIS_URL) {
+    server.use(prerender.redisCache());
+}
 
 server.start();
